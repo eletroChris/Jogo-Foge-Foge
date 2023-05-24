7e19fd5a721096bb02d1597cfdcbee3bd1089379
eletroChris <eletrochris77@gmail.com>
2023-05-24 18:18:54 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Foge Foge
+
+Você encontrará no repositório a implementação em C de um jogo similar ao de Pacman. Esse código é baseado no curso "C:
+recursos avançados da linguagem" da Alura. O jogo não tem interface gráfica e utiliza apenas o terminal.
---
+++ fogefoge.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char **mapa;
+int linhas;
+int colunas;
+
+int main(){
+    
+
+    FILE *f;
+    f=fopen("mapa.txt","r");
+    if(f==0){
+        printf("Erro ao ler o mapa\n\n");
+        exit(1);
+    }
+    fscanf(f,"%d %d",&linhas, &colunas);
+    printf("Linhas: %d Colunas: %d\n", linhas, colunas);
+    mapa = malloc(sizeof(char*)*linhas);
+    for(int i=0;i<linhas;i++){
+        mapa[i]=malloc(sizeof(char*)*(colunas+1));
+    }
+
+    for(int i=0;i<5;i++){
+        fscanf(f,"%s",mapa[i]);
+    }
+    fclose(f);
+
+    for(int i=0;i<5;i++){
+        printf("%s\n",mapa[i]);
+    }
+
+    for(int i =0; i<linhas;i++){
+        free(mapa[i]);
+    }
+    free(mapa);
+}

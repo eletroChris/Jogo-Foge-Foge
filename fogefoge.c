78c4ed821429dbc85638c11bb6b15732c29c441b
eletroChris <eletrochris77@gmail.com>
2023-05-24 18:46:59 -0300

    Refatorando o código através da adição das funções: 'leMapa', 'alocaMapa' e 'liberaMapa'

--- fogefoge.c
+++ fogefoge.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fogefoge.h"
 
 char **mapa;
 int linhas;
 int colunas;
 
+
+
 int main(){
     
+    leMapa();
 
+    for(int i=0;i<5;i++){
+        printf("%s\n",mapa[i]);
+    }
+
+    liberaMapa();    
+}
+
+void liberaMapa(){
+    for(int i =0; i<linhas;i++){
+        free(mapa[i]);
+    }
+    free(mapa);
+}
+
+void alocaMapa(){
+    mapa = malloc(sizeof(char*)*linhas);
+    for(int i=0;i<linhas;i++){
+        mapa[i]=malloc(sizeof(char*)*(colunas+1));
+    }
+}
+
+void leMapa(){
     FILE *f;
     f=fopen("mapa.txt","r");
     if(f==0){
@@ -15,23 +41,11 @@ int main(){
         exit(1);
     }
     fscanf(f,"%d %d",&linhas, &colunas);
-    printf("Linhas: %d Colunas: %d\n", linhas, colunas);
-    mapa = malloc(sizeof(char*)*linhas);
-    for(int i=0;i<linhas;i++){
-        mapa[i]=malloc(sizeof(char*)*(colunas+1));
-    }
+    
+    alocaMapa();
 
     for(int i=0;i<5;i++){
         fscanf(f,"%s",mapa[i]);
     }
     fclose(f);
-
-    for(int i=0;i<5;i++){
-        printf("%s\n",mapa[i]);
-    }
-
-    for(int i =0; i<linhas;i++){
-        free(mapa[i]);
-    }
-    free(mapa);
 }
